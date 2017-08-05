6ee104b91d72bb90f8e779561917a4ebbcef41e1
VisualDudek <dudek.v@gmail.com>
2017-08-05 20:45:19 +0200

    Katalog z zadanimi z CODEVAL

---
+++ CODEVAL/ODD_NUMBERS.py
@@ -0,0 +1,12 @@
+# CHALLENGE DESCRIPTION:
+# Print the odd numbers from 1 to 99.
+#
+# INPUT SAMPLE:
+#
+# There is no input for this program.
+#
+# OUTPUT SAMPLE:
+#
+# Print the odd numbers from 1 to 99, one number per line.
+
+for x in range(1,99+1,2): print(x)
---
+++ Sololearn.py
@@ -0,0 +1,17 @@
+# Newlines
+# Create string with three sets of quotes to escape newlines in string
+print('''Hello
+    World''')
+
+# In-place Operators + - | ^ & / >> % // **
+x=2
+x += 3
+print(x)
+
+x=7
+x %= 5
+print(x)
+
+# Comparison => bez znaczenia (float or int)
+# <= or >= moża także używać do Str
+print(7 > 7.0)
