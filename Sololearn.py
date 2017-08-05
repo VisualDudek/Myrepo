3a8160a4f9f8eb4e3d819a2282e68f65608685bc
VisualDudek <dudek.v@gmail.com>
2017-08-05 23:18:37 +0200

    Zadanie SWAP_NUMBERS
    Nowe zadanie w katalogu CODEVAL

--- Sololearn.py
+++ Sololearn.py
@@ -15,3 +15,23 @@ print(x)
 # Comparison => bez znaczenia (float or int)
 # <= or >= moża także używać do Str
 print(7 > 7.0)
+
+# KALKULATOR ------------
+while True:
+    print('''
+    Opcje:
+    Enter "add"
+    Enter "multiply"
+    Enter "quit"''')
+    user_input = input('--> ')
+
+    if user_input == 'quit':
+        break
+    elif user_input == 'add':
+        num1 = float(input('1th number --> '))
+        num2 = float(input('2th number --> '))
+        print('Result: ' + str(num1 + num2) )
+    elif user_input == 'multiply':
+        print()
+    else:
+        print('Unknow input')
